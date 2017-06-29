f1e20f86753310b6ab2e019bf78b5586c151b90c
ellen87006 <ellen87006@gmail.com>
2017-06-29 21:14:47 +0800

    final

---
+++ src/laa-wifi-coexistence/experiments/Adaptive-LBT-nack40notfair_f/images/scripts/indoor_-82_0_ftpLambda_0_8_operatorB_latency_eps.gnuplot
@@ -0,0 +1,42 @@
+ 
+ set style line 1 pt 4 lt 1 
+ set style line 2 pt 7 lt 2 
+ set style increment 
+ 
+ set pointsize 2 
+ set grid
+ 
+ set key bottom right 
+ set term postscript eps enhanced   color   
+ set output "images/ps/indoor_-82_0_ftpLambda_0_8_operatorB_latency.eps" 
+   set xlabel "Flow number CDF"
+ set ylabel "Latency [ms]"
+  
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ 
+ unset title
+ plot [0:1][0:200] "results/cdf_latency_eD_-82.0_ftpLambda_0.8_cellA_Laa_rule_all_fair_B" using ($2):($1)  with linespoints ls 1  title "Scenario 6"  , "results/cdf_latency_eD_-82.0_ftpLambda_0.8_cellA_Laa_rule_all_B" using ($2):($1)  with linespoints ls 2  title "Scenario 5"  , "results/cdf_latency_eD_-82.0_ftpLambda_0.8_cellA_Laa_rule_any_B" using ($2):($1)  with linespoints ls 3  title "Scenario 4"  , "results/cdf_latency_eD_-82.0_ftpLambda_0.8_cellA_Laa_rule_nacks10_B" using ($2):($1)  with linespoints ls 4  title "Scenario 3"  , "results/cdf_latency_eD_-82.0_ftpLambda_0.8_cellA_Laa_rule_nacks80_B" using ($2):($1)  with linespoints ls 5  title "Scenario 2"  , "results/cdf_latency_eD_-82.0_ftpLambda_0.8_cellA_Wifi_rule_nacks10_B" using ($2):($1)  with linespoints ls 6  title "Scenario 1 "  
---
+++ src/laa-wifi-coexistence/experiments/Adaptive-LBT-nack40notfair_f/run.sh
@@ -0,0 +1,108 @@
+source config
+
+outputDir=`pwd`/results
+
+control_c()
+{
+  echo "exiting"
+  exit $?
+}
+
+trap control_c SIGINT
+
+if test ! -f ../../../../waf ; then
+  echo "please run this program from within the directory `dirname $0`, like this:"
+  echo "cd `dirname $0`"
+  echo "./`basename $0`"
+  exit 1
+fi
+
+set -x
+set -o errexit
+
+# If the script is run directly, then we need to clean results directory
+# Otherwise if it is run from a run-in-parallel script this is already done
+if [[ $# -eq 0 ]] ; then
+  outputDir=`pwd`/results
+  mkdir -p "${outputDir}"
+  rm -f "${outputDir}"/laa_wifi_indoor_*_operator?
+  rm -f "${outputDir}"/time_stats
+  rm -f "${outputDir}"/laa_wifi_indoor_*_log
+else
+  lbtTxopList="$1"
+fi
+
+# need this as otherwise waf won't find the executables
+cd ../../../../
+Mintxop=8800000.0ns
+Maxtxop=17400000.0ns
+
+for ftpLambda in 0.35 0.5 0.65 0.8 0.95;do
+  for MaxCws in 64 ; do
+  # Energy detection threshold for LAA
+      for MinCws in 19 ; do
+        for lbtTxop in 8.8 ; do 
+            for cell in Laa ; do
+              for rules in all  ;do 
+                    # Make the simulation duration inversly proportional to ftpLambda
+                    duration=$(echo "$base_duration/$ftpLambda" | bc)
+                    simTag="eD_${energyDetection}_ftpLambda_${ftpLambda}_cellA_${cell}_rule_${rules}_fair"
+                    /usr/bin/time -f '%e %U %S %K %M %x %C' -o "${outputDir}"/time_stats -a \
+                    ./waf --run laa-wifi-indoor --command="%s --ftpLambda=${ftpLambda} --ns3::LbtAccessManager::MaxTxop=${Maxtxop} --ns3::LbtAccessManager::MinTxop=${Mintxop} --lbtTxop=${lbtTxop} --ns3::LbtAccessManager::DeferTime=${initalCCA} --ns3::LbtAccessManager::MaxCw=${MaxCws} --ns3::LbtAccessManager::MinCw=${MinCws} --cellConfigA=${cell} --cellConfigB=Wifi --logWifiRetries=1 --logWifiFailRetries=1 --logPhyArrivals=0 --transport=${transports} --duration=${duration} --cwUpdateRule=${rules} --logHarqFeedback=1 --logTxops=1 --logCwChanges=1 --logBackoffChanges=1 --laaEdThreshold=${energyDetection} --simTag=${simTag} --outputDir=${outputDir} --voiceEnabled=${voiceEnabled} --dropPackets=${dropPackets} --ns3::TcpSocket::SegmentSize=${tcpSegSize} --tcpRlcMode=${tcpRlcMode} --ns3::LteEnbRrc::DefaultTransmissionMode=${laaTxMode} --RngRun=${RngRun}"
+                done
+              done
+          done
+      done
+  done
+done
+for ftpLambda in 0.35 0.5 0.65 0.8 0.95;do
+  for MaxCws in 64 ; do
+  # Energy detection threshold for LAA
+      for MinCws in 24 ; do
+        for lbtTxop in 9.0 ; do 
+            for cell in Laa ; do
+              for rules in all any  ;do 
+                    # Make the simulation duration inversly proportional to ftpLambda
+                    duration=$(echo "$base_duration/$ftpLambda" | bc)
+                    simTag="eD_${energyDetection}_ftpLambda_${ftpLambda}_cellA_${cell}_rule_${rules}"
+                    /usr/bin/time -f '%e %U %S %K %M %x %C' -o "${outputDir}"/time_stats -a \
+                    ./waf --run laa-wifi-indoor --command="%s --ftpLambda=${ftpLambda} --ns3::LbtAccessManager::DeferTime=${initalCCA}  --cellConfigA=${cell} --cellConfigB=Wifi --logWifiRetries=1 --logWifiFailRetries=1 --logPhyArrivals=0 --transport=${transports} --duration=${duration} --cwUpdateRule=${rules} --logHarqFeedback=1 --logTxops=1 --logCwChanges=1 --logBackoffChanges=1 --laaEdThreshold=${energyDetection} --simTag=${simTag} --outputDir=${outputDir} --voiceEnabled=${voiceEnabled} --dropPackets=${dropPackets} --ns3::TcpSocket::SegmentSize=${tcpSegSize} --tcpRlcMode=${tcpRlcMode} --ns3::LteEnbRrc::DefaultTransmissionMode=${laaTxMode} --RngRun=${RngRun}"
+                done
+       #       done
+
+         # done
+      #done
+  done
+done
+for ftpLambda in 0.35 0.5 0.65 0.8 0.95;do
+  for MaxCws in 32 ; do
+  Energy detection threshold for LAA
+      for MinCws in 5 ; do
+        for lbtTxop in 9.0 ; do 
+            for cell in Laa ; do
+              for rules in  nacks10 nacks80;do 
+                    # Make the simulation duration inversly proportional to ftpLambda
+                    duration=$(echo "$base_duration/$ftpLambda" | bc)
+                    simTag="eD_${energyDetection}_ftpLambda_${ftpLambda}_cellA_${cell}_rule_${rules}"
+                    /usr/bin/time -f '%e %U %S %K %M %x %C' -o "${outputDir}"/time_stats -a \
+                    ./waf --run laa-wifi-indoor --command="%s --ftpLambda=${ftpLambda} --ns3::LbtAccessManager::DeferTime=${initalCCA} --cellConfigA=${cell} --cellConfigB=Wifi --logWifiRetries=1 --logWifiFailRetries=1 --logPhyArrivals=0 --transport=${transports} --duration=${duration} --cwUpdateRule=${rules} --logHarqFeedback=1 --logTxops=1 --logCwChanges=1 --logBackoffChanges=1 --laaEdThreshold=${energyDetection} --simTag=${simTag} --outputDir=${outputDir} --voiceEnabled=${voiceEnabled} --dropPackets=${dropPackets} --ns3::TcpSocket::SegmentSize=${tcpSegSize} --tcpRlcMode=${tcpRlcMode} --ns3::LteEnbRrc::DefaultTransmissionMode=${laaTxMode} --RngRun=${RngRun}"
+                done
+            #  done
+
+          #done
+      #done
+  done
+done
+cd ~/ns-3-lbt
+for ftpLambda in 0.35 0.5 0.65 0.8 0.95;do
+  # Energy detection threshold for LAA
+            for cell in Wifi ; do
+              for rules in nacks10 ;do 
+                    # Make the simulation duration inversly proportional to ftpLambda
+                    duration=$(echo "$base_duration/$ftpLambda" | bc)
+                    simTag="eD_${energyDetection}_ftpLambda_${ftpLambda}_cellA_${cell}_rule_${rules}"
+                    /usr/bin/time -f '%e %U %S %K %M %x %C' -o "${outputDir}"/time_stats -a \
+                    ./waf --run laa-wifi-indoor --command="%s --ftpLambda=${ftpLambda}   --cellConfigA=${cell} --cellConfigB=Wifi --logWifiRetries=1 --logWifiFailRetries=1 --logPhyArrivals=0 --transport=${transports} --duration=${duration} --cwUpdateRule=${rules} --logHarqFeedback=1 --logTxops=1 --logCwChanges=1 --logBackoffChanges=1 --laaEdThreshold=${energyDetection} --simTag=${simTag} --outputDir=${outputDir} --voiceEnabled=${voiceEnabled} --dropPackets=${dropPackets} --ns3::TcpSocket::SegmentSize=${tcpSegSize} --tcpRlcMode=${tcpRlcMode} --ns3::LteEnbRrc::DefaultTransmissionMode=${laaTxMode} --RngRun=${RngRun}"
+                done
+              done
+done
